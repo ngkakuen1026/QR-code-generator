ad92b42be101beca89947ec47b5f337cabd0a36f
Alan Ng <nkk991026@gmail.com>
2024-11-09 00:02:40 +0800

    Complete QR Generator & Modify UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import {QRCodeSVG} from 'qrcode.react';
+import { useState } from 'react';
+
 function App() {
+
+  const [url, setUrl] = useState('https://reactjs.org');
+
+  const handleInputChange = (e) => {
+    setUrl(e.target.value);
+  };
+
   return (
     <div className="min-h-screen px-20 py-10 bg-gray-600">
-      <div className="text-center m-8">
+      <div className="text-center">
         <h1 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">QR</span> Code Generator
         </h1>
@@ -11,22 +21,18 @@ function App() {
           code.
         </h1>
       </div>
-      <div className="flex">
+      <div className="p-10">
           <input 
               type="text" 
               id="large-input"
               className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              value={url}
+              onChange={handleInputChange}
           />
-          <button className="bg-gray-900 hover:bg-gray-500 py-4 px-8 border-gray-900 rounded">
-              <span className="text-white font-extrabold">Submit</span>
-          </button>
       </div>
-      <div>
-
+      <div className="flex justify-center">
+        <QRCodeSVG value={url} size={384} />
       </div>
-
-
-
     </div>
   )
 }
